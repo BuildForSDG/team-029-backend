1b18c9fda2e74bdaf3d860baced5bdbf87253c2a
marusoft <alimi.kmaruf@gmail.com>
2020-05-22 12:32:52 +0100

    ch(accidents-table): populate sql files to create the required columns

--- migrations/sqls/20200522104020-add-accidents-up.sql
+++ migrations/sqls/20200522104020-add-accidents-up.sql
@@ -1,1 +1,9 @@
 /* Replace with your SQL commands */
+-- id. |. road_id | description | created_at | updated_at
+CREATE TABLE IF NOT EXISTS accidents (
+    id VARCHAR(50) NOT NULL, 
+    road_id VARCHAR REFERENCESroad(id),
+    description VARCHAR(250), 
+    created_at TIMESTAMPTZ DEFAULT NOW(),
+    updated_at TIMESTAMPTZ DEFAULT NOW()
+);
