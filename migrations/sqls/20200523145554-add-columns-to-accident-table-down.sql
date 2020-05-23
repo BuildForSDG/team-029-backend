07bda0d6839fece43c6734b70d492dba914feb5c
Sikiru Moshood <msikiruscholarship@gmail.com>
2020-05-23 16:14:03 +0100

    chore(database): pull accidents schema and add new columns

---
+++ migrations/sqls/20200523145554-add-columns-to-accident-table-down.sql
@@ -0,0 +1,10 @@
+/* Replace with your SQL commands */
+ALTER TABLE accidents DROP COLUMN IF EXISTS status;
+ALTER TABLE accidents DROP COLUMN IF EXISTS warden_user_id;
+ALTER TABLE accidents DROP COLUMN IF EXISTS ussd_session_id;
+ALTER TABLE accidents DROP COLUMN IF EXISTS victim_phone_number;
+ALTER TABLE accidents DROP COLUMN IF EXISTS longitude;
+ALTER TABLE accidents DROP COLUMN IF EXISTS latitude;
+ALTER TABLE accidents DROP COLUMN IF EXISTS address;
+
+
---
+++ migrations/sqls/20200523145554-add-columns-to-accident-table-up.sql
@@ -0,0 +1,10 @@
+/* Replace with your SQL commands */
+ALTER TABLE accidents ADD COLUMN status VARCHAR(200) DEFAULT('pending');
+ALTER TABLE accidents ADD COLUMN warden_user_id VARCHAR(50);
+ALTER TABLE accidents ADD COLUMN ussd_session_id VARCHAR(200);
+ALTER TABLE accidents ADD COLUMN victim_phone_number VARCHAR(20);
+ALTER TABLE accidents ADD COLUMN longitude NUMERIC;
+ALTER TABLE accidents ADD COLUMN latitude NUMERIC;
+ALTER TABLE accidents ADD COLUMN address TEXT;
+
+
