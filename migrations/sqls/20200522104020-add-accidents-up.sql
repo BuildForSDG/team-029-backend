7c830d0cb8e5cc9bfbca1d9815e53232e5c47424
marusoft <alimi.kmaruf@gmail.com>
2020-05-23 03:51:03 +0100

    ch(accidents-table):setup and populate accidents table

--- migrations/sqls/20200522104020-add-accidents-up.sql
+++ migrations/sqls/20200522104020-add-accidents-up.sql
@@ -1,8 +1,8 @@
 /* Replace with your SQL commands */
--- id. |. road_id | description | created_at | updated_at
+
 CREATE TABLE IF NOT EXISTS accidents (
     id VARCHAR(50) NOT NULL, 
-    road_id VARCHAR REFERENCESroad(id),
+    road_id VARCHAR(50),
     description VARCHAR(250), 
     created_at TIMESTAMPTZ DEFAULT NOW(),
     updated_at TIMESTAMPTZ DEFAULT NOW()
