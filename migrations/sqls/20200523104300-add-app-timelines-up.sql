5ab0e7636d28ac7af231525d3a75627b18329c01
marusoft <alimi.kmaruf@gmail.com>
2020-05-28 20:59:07 +0100

    ch(app-timelines): change id data type

--- migrations/sqls/20200523104300-add-app-timelines-up.sql
+++ migrations/sqls/20200523104300-add-app-timelines-up.sql
@@ -1,7 +1,7 @@
 /* Replace with your SQL commands */
 CREATE TABLE IF NOT EXISTS app_timelines (
-    id VARCHAR(50) NOT NULL, 
-    admin_id VARCHAR(50),
+    id SERIAL PRIMARY KEY NOT NULL, 
+    admin_id INTEGER NOT NULL,
     description VARCHAR(250), 
     created_at TIMESTAMPTZ DEFAULT NOW(),
     updated_at TIMESTAMPTZ DEFAULT NOW()
